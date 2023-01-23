9e6462330034b46b383ea6e35939bd059cc7ce10
Zaid Loni <96578621+zaidloni@users.noreply.github.com>
2023-01-23 10:02:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+let str;
+document.getElementById("evaluatedText").value = ""; // initially string is empty so count is also empty
+document.addEventListener("keyup", function () {
+	  str = document.getElementById("evaluatedText").value; //here this value will return string
+ let x = str.split(' ')
+	x= x.filter(item => item);
+	console.log(x)
+	document.querySelector("#wordCount").textContent = x.length;
